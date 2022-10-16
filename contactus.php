198651fb6382dfb8d8c5a3a1d752d123712d0ed4
QznadBel <115936123+QznadBel@users.noreply.github.com>
2022-10-16 15:44:47 +0100

    Add files via upload

--- contactus.php
+++ contactus.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
 	<link rel="stylesheet" href="style.css">
-	<title>Cinemates</title>
 </head>
 <body>
-	<div class="ban1">
-	<div class="lien">
-	<ul>
-		<li><a href="cinemates.html">Home</a></li>
-		<li><a href="#">More</a></li>
-		<li><a href="contactus.html">Contact us</a></li>
-	</ul>
-    </div>
-    <div class="contact-section">
+	<?php
+	if(!empty($_POST["send"]){
+		$YourName = $_POST["YourName"];
+		$YourEmail = $_POST["YourEmail"];
+		$YourMessage = $_POST["YourMessage"];
+		$toEmail = $_POST["yassineinfo800@gmail.com"];
+
+		$mailHeaders = "Name: " . $YourName . 
+		"\r\n Email: " . $YourEmail .
+		"\r\n Your Message: " . $$YourMessage .
+		"\r\n Email: " . $YourEmail . "\r\n"
+
+		if(mail($toEmail, $YourName, $mailHeaders)){
+			$message = "Your informations are received"
+		}
+
+	} 
+	 ?>
+<div class="contact-section">
         <h1>Contact Us</h1>
     <form class="content-form" action="index.html" action="post">
     	<input type="text" class="contact-form-text" placeholder="Your name" name="YourName">
     	<input type="email" class="contact-form-text" id="gmail" placeholder="Your email" name="YourEmail">
     	<textarea class="contact-form-text" placeholder="Your message" name="YourMessage"></textarea>
     	<input type="submit" class="contact-form-btn" value="Send" name="send">
+    	<?php if(!empty($message)){?>
     </div>
+	<?php } ?>
     </form>
-    </div>
 </body>
 </html>
